27085db97ad1b3438f531c4c5e4d2cab2443fa30
20100922-github <20100922-github@schleinkofer.name>
2013-04-11 16:42:28 +0200

    initial commit (empty rails app)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OctopussE::Application.config.secret_token = '9101ca16e8b7294421e9f5087d3aaca69c579f321f5b51ece93da50123c4b7c45cdd5113d918746209cda69c26472e018e3314920dfa0fd0c49b331e639be21a'
