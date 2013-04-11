d96a1c527ea5203c7b078e29a6755ed78c921ecb
20100922-github <20100922-github@schleinkofer.name>
2013-04-11 17:30:42 +0200

    basic setup of home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+    if (Folder.count == 0)
+      @root = Folder.new
+      @root.name = '*'
+      @root.save
+    end
+    @root = Folder.first.root
+    @descendants = @root.descendants
+    @root_bookmarks = Bookmark.where(:folder_id => @root)
   end
 end
