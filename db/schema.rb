e344e318ddf91478d4b3ea4a0fb5ec50a65eae19
20100922-github <20100922-github@schleinkofer.name>
2013-04-14 10:13:05 +0200

    use username instead of email for authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130412175117) do
+ActiveRecord::Schema.define(:version => 20130414053231) do
 
   create_table "bookmarks", :force => true do |t|
     t.string   "name"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(:version => 20130412175117) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
